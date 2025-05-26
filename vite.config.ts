8f630cd244d89c5df5f36edd502bf78f8b0b19df
aabbPhD <aabb22phd@mail.ru>
2025-05-26 23:16:17 +0300

    'again'

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: 'GroceryApp',
-=======
-  base: '/GroceryApp/',
->>>>>>> 69c3c2b0e7f79b8a8b17df0a3fe24334f998f4c3
 })
