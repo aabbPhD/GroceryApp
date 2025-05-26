f5e9b02e43fa39446bf59d703154e987d9a79214
aabbPhD <aabb22phd@mail.ru>
2025-05-27 00:24:23 +0300

    'final'

--- src/utils/image-util.ts
+++ src/utils/image-util.ts
@@ -1,9 +1,3 @@
 export default function getImageURL(name: string): string {
-  //GitHub Pages
-  if (import.meta.env.PROD) {
-    return `${import.meta.env.BASE_URL}/assets/img/products/${name}`;
-  }
-  
-  //Для разработки
-  return new URL(`../assets/img/products/${name}`, import.meta.url).href;
+  return new URL(`../assets/img/products/${name}`, import.meta.url).href
 }
